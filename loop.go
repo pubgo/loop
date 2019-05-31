d01fa9ee6a997d352c6616095ec9a98be447048f
kooksee <kooksee@163.com>
2019-05-31 12:19:48 +0800

    update

--- loop.go
+++ loop.go
@@ -13,8 +13,9 @@ func Loop(fn func() interface{}, efn func(err error)) (v interface{}) {
 			if _err := _KTry(efn, err); _err != nil {
 				log.Fatalln(_err.(*_KErr).StackTrace())
 			}
-			return
+			continue
 		}
+		return
 	}
 }
 
